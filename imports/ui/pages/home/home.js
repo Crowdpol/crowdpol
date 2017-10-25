c4c2e6cf9868911f7b3630bb8e0e8b5ef96f111f
Trudie <tspangenberg1@gmail.com>
2017-10-25 13:27:35 +0200

    Fix more merge conflicts

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,5 +1,2 @@
 import './home.html';
 
-import '../../components/signup/signup.js';
-import '../../components/authenticated/index.js';
-
--- imports/ui/pages/login/login.js
+++ imports/ui/pages/login/login.js
@@ -1,1 +1,3 @@
 import './login.html';
+import '../../components/signup/signup.js';
+import '../../components/authenticated/index.js';
