e55c010cd650cc8a2f7acda8ef87821678d365c6
Trudie <tspangenberg1@gmail.com>
2017-10-26 17:20:19 +0200

    small cleanups

--- server/methods/send-contact-message.js
+++ server/methods/send-contact-message.js
@@ -2,7 +2,7 @@ Meteor.methods({
   sendContactMessage(message) {
     Meteor.defer(() => {
       Email.send({
-        to: 'Common Democracy <tspangenberg1@gmail.com>',
+        to: 'Common Democracy <info@commondemocracy.org>',
         from: `${message.name} ${message.email}`,
         subject: `${message.name} sent a message!`,
         text: message.message,
