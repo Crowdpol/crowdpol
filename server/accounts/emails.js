6611941f618853585c0df92b1613359bd15334b9
Trudie <tspangenberg1@gmail.com>
2017-10-27 11:49:11 +0200

    add email translations

--- server/accounts/emails.js
+++ server/accounts/emails.js
@@ -3,13 +3,13 @@ Accounts.emailTemplates.from     = "Common Democracy <info@commondemocracy.com>"
 
 Accounts.emailTemplates.verifyEmail = {
   subject() {
-    return "Common Democracy: Please Verify Your Email";
+    return TAPi18n.__('emails.verify-email.subject');
   },
   text( user, url ) {
     let emailAddress   = user.emails[0].address,
         urlWithoutHash = url.replace( '#/', '' ),
         supportEmail   = "info@commondemocracy.com",
-        emailBody      = `To verify your email address (${emailAddress}) visit the following link:\n\n${urlWithoutHash}\n\n If you did not request this verification, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
+        emailBody      = TAPi18n.__('emails.verify-email.body', {emailAddress: emailAddress, url: urlWithoutHash, supportEmail: supportEmail});
 
     return emailBody;
   }
@@ -17,13 +17,13 @@ Accounts.emailTemplates.verifyEmail = {
 
 Accounts.emailTemplates.resetPassword = {
   subject() {
-    return "Common Democracy: Reset Your Password";
+    return TAPi18n.__('emails.reset-password.subject');
   },
   text( user, url ) {
     let emailAddress   = user.emails[0].address,
         urlWithoutHash = url.replace( '#/', '' ),
         supportEmail   = "info@commondemocracy.com",
-        emailBody      = `To reset your password, simply click the link below: \n\n${urlWithoutHash}\n\n If you did not request to change your password, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
+        emailBody      = TAPi18n.__('emails.reset-password.body', {url: urlWithoutHash, supportEmail: supportEmail});
 
     return emailBody;
   }
