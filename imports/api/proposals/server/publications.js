491f4654732f22269693e4a548726e813a9b1d08
Trudie <tspangenberg1@gmail.com>
2017-11-11 16:02:48 +0200

    tabs for vote/authored proposals

--- imports/api/proposals/server/publications.js
+++ imports/api/proposals/server/publications.js
@@ -8,3 +8,19 @@ Meteor.publish('proposals.all', function() {
 Meteor.publish('proposals.one', function(id) {
   return Proposals.find({_id: id});
 });
+
+Meteor.publish('proposals.open', function() {
+	return Proposals.find({endDate:{"$lte": new Date()}, stage: 'live'});
+});
+
+Meteor.publish('proposals.closed', function() {
+	return Proposals.find({endDate:{"$gte": new Date()}, stage: 'live'});
+});
+
+Meteor.publish('proposals.author', function(authorId) {
+  return Proposals.find({authorId: authorId});
+});
+
+Meteor.publish('proposals.invited', function() {
+  return Proposals.find();
+});
--- imports/ui/pages/proposals/proposalsList.js
+++ imports/ui/pages/proposals/proposalsList.js
@@ -4,14 +4,23 @@ import { Proposals } from '../../../api/proposals/Proposals.js'
 Template.ProposalsList.onCreated(function () {
   var self = this;
   self.autorun(function(){
-    self.subscribe('proposals.all');
+    self.subscribe('proposals.open');
+    self.subscribe('proposals.closed');
+    self.subscribe('proposals.author');
+    self.subscribe('proposals.invited');
   });
 });
 
 Template.ProposalsList.helpers({
-  proposals: function() {
-    return Proposals.find({}, {transform: transformProposal, sort: {createdAt: -1}});
+  closedProposals: function() {
+    return Proposals.find({endDate:{"$lte": new Date()}, stage: 'live'}, {transform: transformProposal, sort: {createdAt: -1}});
   },
+  openProposals: function() {
+    return Proposals.find({endDate:{"$gte": new Date()}, stage: 'live'}, {transform: transformProposal, sort: {createdAt: -1}});
+  },
+  myProposals: function(){
+    return Proposals.find({authorId: Meteor.userId()});
+  }
 });
 
 Template.ProposalsList.events({
