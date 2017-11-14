8e5fb9bcadffde2f46934eb0d2defed2150be66d
Trudie <tspangenberg1@gmail.com>
2017-11-14 21:49:22 +0200

    fix the pesky hamburger

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -5,4 +5,17 @@ import '../footer/footer.js'
 
 Template.App_body.onCreated(function() {
   //$('.mdl-layout').MaterialLayout.toggleDrawer();
+  //showDrawer = Meteor.user()
+  var user = Meteor.user();
+  if (user){
+  	showDrawer = true;
+  } else {
+  	showDrawer = false;
+  }
+});
+
+Template.App_body.helpers({
+	showDrawer: function(){
+		return showDrawer;
+	}
 });
