e5dec156dcc436ce52a899f03537a2ffc9453f04
Trudie <tspangenberg1@gmail.com>
2017-11-15 11:58:49 +0200

    header role switch

--- imports/ui/layouts/header/header.js
+++ imports/ui/layouts/header/header.js
@@ -2,11 +2,48 @@ import { Session } from 'meteor/session';
 
 import './header.html';
 
+Template.Header.onCreated(function(){
+  var self = this;
+  var user = Meteor.user();
+
+  if (user && user.roles){
+    var currentRole = Session.get('currentUserRole');
+    if (!currentRole){
+      Session.set('currentUserRole', Meteor.user().roles[0]);
+    }
+  }
+});
+
 Template.Header.helpers({
 	lang() {
 		var str = Session.get("i18n_lang")
 		return str.toUpperCase();
-	}
+	},
+
+  userHasMultipleRoles(){
+    var user = Meteor.user();
+    var userRoles = user.roles;
+    if (user && userRoles) {
+      var roles = getMenuRoles(userRoles);
+      return roles.length > 1;
+    }
+    return false;
+  },
+
+  roles(){
+    var userRoles = Meteor.user().roles;
+    var roles = getMenuRoles(userRoles);
+    //Capitalise first letter of role name
+    return _.map(roles, function(role){ return role.charAt(0).toUpperCase() + role.slice(1);; });
+  },
+
+  currentRole(){
+    return Session.get('currentUserRole');
+  },
+
+  isCurrentRole(role){
+    return (role == Session.get('currentUserRole'));
+  }
 });
 
 Template.Header.events({
@@ -19,5 +56,13 @@ Template.Header.events({
     event.preventDefault();
 	Meteor.logout();
   },
+  'click .role-menu-item' : function(){
+    Session.set('currentUserRole', event.target.dataset.role);
+  }
 });
+
+function getMenuRoles(userRoles){
+  var menuRoles = ['individual', 'delegate', 'candidate'];
+  return _.intersection(userRoles, menuRoles);
+}
 
