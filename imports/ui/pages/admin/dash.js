bbc6d5a5461ecffdf4fd76440d14faf53d31c5be
Trudie <tspangenberg1@gmail.com>
2017-11-16 04:48:32 +0200

    Check if user is admin on admin templates

--- imports/ui/pages/admin/dash.js
+++ imports/ui/pages/admin/dash.js
@@ -3,3 +3,12 @@ import './tags/tags.js';
 import './approvals/approvals.js';
 import './proposals/proposals.js';
 import './dash.html';
+
+Template.registerHelper('currentUserIsAdmin', function(){
+	var user = Meteor.user();
+	if (user && Roles.userIsInRole(user, ['admin', 'superadmin'])){
+		return true;
+	} else {
+		return false;
+	}
+})
