c975fd11fe56877f99005f4b1a03d1855cd86375
Trudie <tspangenberg1@gmail.com>
2017-11-21 17:17:56 +0200

    fix ranking methods and insert

--- imports/api/ranking/methods.test.js
+++ imports/api/ranking/methods.test.js
@@ -1,17 +1,31 @@
 import { Meteor } from 'meteor/meteor';
 import { assert } from 'meteor/practicalmeteor:chai';
 import { Random } from 'meteor/random';
+import { Factory } from 'meteor/dburles:factory';
+import { fakerSchema } from '../../utils/test-utils/faker-schema/';
 import './methods.js';
+
+const { schema, generateDoc } = fakerSchema;
 
 if (Meteor.isServer) {
   let testRank;
-  beforeEach(function () {
-
-  });
   describe('Rank methods', () => {
+    beforeEach(function () {
+    // create a fake user
+    Factory.define('user', Meteor.users, schema.User);
+    const userId = Factory.create('user')._id
+    // stub Meteor's user method to simulate a logged in user
+    stub = sinon.stub(Meteor, 'userId');
+    stub.returns(userId)
+  });
+
+    afterEach(function () {
+      sinon.restore(Meteor, 'userId');
+    });
+
     it("Add rank", (done) => {
       try {
-        testRank = Meteor.call('addRank', "delegate",Random.id(),Random.id(),Random.fraction());
+        testRank = Meteor.call('addRank', "delegate", Random.id(), Random.fraction());
         done();
       } catch (err) {
         console.log(err);
@@ -21,7 +35,7 @@ if (Meteor.isServer) {
 
     it("Get rank", (done) => {
       try {
-        Meteor.call('getRank', testRank);
+        Meteor.call('getRank', testRank[0]._id);
         done();
       } catch (err) {
         console.log(err);
@@ -31,7 +45,7 @@ if (Meteor.isServer) {
 
     it("Delete rank", (done) => {
       try {
-        Meteor.call('deleteRank', testRank);
+        Meteor.call('deleteRank', testRank[0]._id);
         done();
       } catch (err) {
         console.log(err);
