aa9f468fbbf7b3034b002195f100b352d5dcd284
Brett Jackman <ilovebutternut@gmail.com>
2017-11-26 14:46:16 +0200

    All HTML pages completed

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -25,7 +25,7 @@ Template.Home.events({
 					Bert.alert(error.message, 'danger');
 				} else {
 					//console.log("success");
-					Bert.alert(TAPi18n.__('alerts.mailing-list-signup-success'), 'success');
+					Bert.alert(TAPi18n.__('pages.home.alerts.mailing-list-signup-success'), 'success');
 				}
 			});
 		}
