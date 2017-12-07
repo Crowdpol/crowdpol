e0088f7a2868c7e67279a01772722962f63c7957
Trudie <tspangenberg1@gmail.com>
2017-12-07 17:40:38 +0200

    fix again

--- imports/api/delegateVotes/methods.js
+++ imports/api/delegateVotes/methods.js
@@ -9,29 +9,28 @@ Meteor.methods({
     var delegate = Meteor.user();
     var proposal = Proposals.findOne(voteData.proposalId);
 
-    var existingVote = Meteor.call('getDelegateVoteFor', voteData.proposalId, Meteor.userId());
-    if (existingVote){
-      return false;
-    } else {
-      // ensure the user is logged in
-      if (!delegate)
-        throw new Meteor.Error(401, "You need to login to vote");
-      if (!voteData.vote)
-        throw new Meteor.Error(422, 'Please vote yes or no');
-      if (!proposal)
-        throw new Meteor.Error(422, 'You must vote on a proposal');
-      if (!Roles.userIsInRole(delegate._id, 'delegate'))
-        throw new Meteor.Error(422, 'You must be a delegate to vote as one.');
-      vote = {
-        vote: voteData.vote,
-        proposalId: voteData.proposalId,
-        reason: voteData.reason,
-        delegateId: delegate._id
-      }
+    if (!delegate)
+      throw new Meteor.Error(401, "You need to login to vote");
+    if (!voteData.vote)
+      throw new Meteor.Error(422, 'Please vote yes or no');
+    if (!proposal)
+      throw new Meteor.Error(422, 'You must vote on a proposal');
+    if (!Roles.userIsInRole(delegate._id, 'delegate'))
+      throw new Meteor.Error(422, 'You must be a delegate to vote as one.');
+    vote = {
+      vote: voteData.vote,
+      proposalId: voteData.proposalId,
+      reason: voteData.reason,
+      delegateId: delegate._id
+    }
 
+    var existingVote = Meteor.call('getDelegateVoteFor', voteData.proposalId, Meteor.userId());
+
+    if (existingVote){
+      return DelegateVotes.update({proposalId: voteData.proposalId, delegateId: Meteor.userId()}, {$set: vote});
+    } else {
       return DelegateVotes.insert(vote);
     }
-    
   },
   deleteDelegateVote: function(voteId) {
     check(voteId, String);
