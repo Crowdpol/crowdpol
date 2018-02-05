6ecebaa5b1c9ee933f8a9a9426da626c2a2d98cf
tspangenberg <tspangenberg1@gmail.com>
2018-02-05 10:53:05 +0200

    WIP

---
+++ imports/ui/pages/voting/voting.js
@@ -0,0 +1,145 @@
+import './voting.html'
+import { Proposals } from '../../../api/proposals/Proposals.js'
+import { Votes } from '../../../api/votes/Votes.js'
+
+Template.Voting.onCreated(function () {
+  var self = this;
+
+  self.searchQuery = new ReactiveVar();
+  self.openProposals = new ReactiveVar(true);
+  self.authorProposals = new ReactiveVar(true);
+  Session.set("canVote",true);
+  // Indicate active tab
+  Session.set("allProposals",true);
+  Session.set("myProposals",false);
+
+  self.autorun(function(){
+    self.subscribe('proposals.public', self.searchQuery.get());
+    self.subscribe('proposals.author', self.searchQuery.get());
+    self.subscribe('proposals.invited', Meteor.user().username, self.searchQuery.get());
+  })
+});
+
+Template.Voting.helpers({
+  searching() {
+    return Template.instance().searching.get();
+  },
+  query() {
+    return Template.instance().searchQuery.get();
+  },
+  closedProposals: function() {
+    return Proposals.find({endDate:{"$lte": new Date()}, stage: "live"}, {transform: transformProposal, sort: {endDate: -1}});
+  },
+  openProposals: function() {
+    return Proposals.find({endDate:{"$gte": new Date()}, stage: "live"}, {transform: transformProposal, sort: {endDate: -1}});
+  },
+  myProposals: function(){
+    return Proposals.find({authorId: Meteor.userId()}, {transform: transformProposal, sort: {createdAt: -1}});
+  },
+  invitedProposals: function(){
+    return Proposals.find({invited: Meteor.user().username}, {transform: transformProposal, sort: {createdAt: -1}});
+  },
+  openSelected: function(){
+    return Template.instance().openProposals.get();
+  },
+  authorSelected: function(){
+    return Template.instance().authorProposals.get();
+  },
+});
+
+Template.Voting.events({
+  'keyup #proposal-search' ( event, template ) {
+    let value = event.target.value.trim();
+    template.searchQuery.set(value);
+  },
+  'click #add-new-proposal, click #new-proposals-link': function(event, template){
+    FlowRouter.go('App.proposal.edit', {id: ''});
+  },
+  'click #open-closed-switch': function(event, template){
+    Session.set("canVote",event.target.checked);
+    Template.instance().openProposals.set(event.target.checked);
+  },
+  'click #author-invited-switch': function(event, template){
+    Template.instance().authorProposals.set(event.target.checked);
+  },
+  'click #my-proposals-tab': function(event, template){
+    Session.set("canVote",false);
+    Session.set("myProposals",true);
+    Session.set("allProposals",false);
+  },
+  'click #vote-proposals-tab': function(event, template){
+    Session.set("canVote",Template.instance().openProposals.get());
+    Session.set("myProposals",false);
+    Session.set("allProposals",true);
+  },
+});
+
+function transformProposal(proposal) { 
+  proposal.endDate = moment(proposal.endDate).format('YYYY-MM-DD');
+  return proposal;
+};
+
+Template.VotingCard.onCreated(function () {
+  var self = this;
+  self.autorun(function(){
+    self.subscribe('votes.all');
+  })
+});
+
+Template.VotingCard.helpers({
+  canVote: function() {
+    return Session.get("canVote");
+  },
+  proposalStatus: function(proposal) {
+    // If looking at public proposals, show open/closed
+    if (Session.get('allProposals')){
+      if (new Date(proposal.endDate) > new Date()){
+        return 'Open';
+      } else {
+        return 'Closed';
+      }
+      // If looking at own proposals, show draft/submitted/live
+    } else if (Session.get('myProposals')){
+      var stage = proposal.stage;
+      return stage.charAt(0).toUpperCase() + stage.slice(1);
+    }
+  },
+  userIsAuthor: function(proposalId) {
+    var proposal = Proposals.findOne(proposalId);
+    return proposal.authorId == Meteor.userId();
+  },
+  yesPercentage: function(proposalId) {
+    var yesCount = Votes.find({proposalId: proposalId, vote: 'yes'}).count();
+    var totalCount = Votes.find({proposalId: proposalId}).count();
+    if (totalCount > 0) {
+      return yesCount/totalCount * 100;
+    } else {
+      return 0;
+    }
+  },
+ noPercentage: function(proposalId) {
+    var yesCount = Votes.find({proposalId: proposalId, vote: 'no'}).count();
+    var totalCount = Votes.find({proposalId: proposalId}).count();
+    if (totalCount > 0) {
+      return yesCount/totalCount * 100;
+    } else {
+      return 0;
+    }
+  }
+});
+
+Template.VotingCard.events({
+  'click .delete-proposal-button': function(event, template){
+    var proposalId = event.target.dataset.proposalId;
+
+    if (window.confirm(TAPi18n.__('proposals.list.confirmDelete'))){
+      Meteor.call('deleteProposal', proposalId, function(error){
+        if (error){
+          Bert.alert(error.reason, 'danger');
+        } else {
+          Bert.alert(TAPi18n.__('proposals.list.deletedMessage'), 'success');
+        }
+      });
+    } 
+  },
+});
