09eeb72ede73ef10b143e4b06f1a7a80dff5cbbe
tspangenberg <tspangenberg1@gmail.com>
2018-04-04 13:09:01 +0200

    add modified package again

---
+++ packages/meteor-force-ssl/package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'meteorblackbelt:force-ssl',
+  summary: "Require this application to use HTTPS",
+  version: "1.0.4",
+  documentation: null
+});
+
+Package.onUse(function (api) {
+  api.use('webapp@1.2.0');
+  api.use('underscore@1.0.3');
+  // make sure we come after livedata, so we load after the sockjs
+  // server has been instantiated.
+  api.use('ddp@1.1.0');
+
+  api.addFiles('force_ssl_common.js', ['client', 'server']);
+  api.addFiles('force_ssl_server.js', 'server');
+
+  // Another thing we could do is add a force_ssl_client.js file that
+  // makes sure document.location.protocol is 'https'. If it detected
+  // the code was loaded from a non-localhost non-https site, it would
+  // stop the app from working and pop up an error box or something.
+});
