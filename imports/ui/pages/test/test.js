e343600c7e99992a68fc6e7a28b866dca8a79aae
ilovebutternut <ilovebutternut@gmail.com>
2019-07-08 17:05:38 +0200

    delegate issue fixed

--- imports/ui/pages/test/test.js
+++ imports/ui/pages/test/test.js
@@ -2,30 +2,14 @@ import './test.html';
 import './test.scss';
 
 Template.Test.onCreated(function(){
-  var wrap = $(".page-content");
-
-  wrap.on("scroll", function(e) {
-    console.log("top:" + this.scrollTop)
-
-  });
-  addEvent(window, "resize", function(event) {
-    console.log('resized');
-    var doc = document.documentElement;
-    var left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
-    var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    console.log("top: " + top);
-    console.log("left: " + left);
-    console.log("w: " + w);
-    console.log("h: " + h);
-    console.log($("#site-wrapper-div").height());
-  });
 
 });
 
 Template.Test.onRendered(function(){
-
+  Meteor.defer(function(){
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+  });
 });
 
 Template.Test.events({
@@ -34,16 +18,3 @@ Template.Test.events({
 Template.Test.helpers({
 
 });
-
-var addEvent = function(object, type, callback) {
-  console.log("addEvent called");
-  if (object == null || typeof(object) == 'undefined') return;
-  if (object.addEventListener) {
-      object.addEventListener(type, callback, false);
-  } else if (object.attachEvent) {
-      object.attachEvent("on" + type, callback);
-  } else {
-      object["on"+type] = callback;
-  }
-
-};
