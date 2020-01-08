2a4891c793ce9378d8cccc1024f75db9bafd88e7
Timothy Olsson <tim@Timothys-MacBook-Pro.local>
2020-01-08 20:52:09 +0200

    latest header changes, part 3

--- imports/ui/pages/landing/landing.js
+++ imports/ui/pages/landing/landing.js
@@ -1,3 +1,20 @@
 import './landing.html';
 //import './landing_files/app.js" type="text/javascript"></script>
 //import './landing.scss';
+
+
+Template.Landing.events({
+  'click .menu-scroll-link': function(event, template){
+    event.preventDefault();
+    console.log($(this).hasClass( "active" ));
+    let anchorId = event.currentTarget.dataset.anchor;
+    scrollTo(anchorId);
+  }
+});
+
+
+function scrollTo(elementId){
+  $('html, body').animate({
+    scrollTop: $(elementId).offset().top
+  }, 2000);
+}
